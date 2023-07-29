1ce0e2948e723b1974cd9e640ff7ec10a6561e08
Govind Saini <79481087+Govindfw220955@users.noreply.github.com>
2023-07-30 00:08:09 +0530

    Update sketch4.py

--- sketch4.py
+++ sketch4.py
@@ -1,4 +1,11 @@
+# Imported the required libraries from the sketchpy library
+
 from sketchpy import library
 
+# For this sketch we took inbuild sketch module of Dr. APJ Abdul Kalam Sir.
+
 obj= library.apj()
+
+# run the sketch program by using draw function.
+
 obj.draw()
