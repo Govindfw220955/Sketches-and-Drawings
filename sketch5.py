3c892fcde367b36338278a11e516e4c791f4302d
Govind Saini <79481087+Govindfw220955@users.noreply.github.com>
2023-07-30 00:04:04 +0530

    Update sketch5.py

--- sketch5.py
+++ sketch5.py
@@ -1,5 +1,13 @@
+# Create new file in which we need to print sketch from our raw data image with the png file.
+# Import libraries which is required for this canvas.
 from sketchpy import canvas
+
+# Import our Raw data image from our resources
+
 jb = canvas.sketch_from_image(r"D:\1270014.png")
+
+# Add threshold (change the quantity of black and white color)in the image which is imported from our local drive location
+
 jb.draw(threshold=150)
 
 
