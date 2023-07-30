7cd6028ea9f952ebba33ca962ad91011bec54e23
Govind Saini <79481087+Govindfw220955@users.noreply.github.com>
2023-07-30 11:56:35 +0530

    Update sketch1.py

--- sketch1.py
+++ sketch1.py
@@ -1,4 +1,8 @@
+# Imported the required libraries from the sketchpy library
 from sketchpy import library
 
+# For this sketch we took inbuild sketch module of RDJ.
 obj= library.rdj()
+
+# run the sketch program by using draw function.
 obj.draw()
