f571e75fae7607f30c32f53d34b50c581e5286ee
Govind Saini <79481087+Govindfw220955@users.noreply.github.com>
2023-07-30 11:39:21 +0530

    Update sketch3.py

--- sketch3.py
+++ sketch3.py
@@ -1,4 +1,7 @@
+# Import the required libraries of sketchpy as canvas
 from sketchpy import canvas
+# import the raw image in svg format
 sk = canvas.sketch_from_svg(r"D:\2023_07_21 (1).svg")
+#  print the image which you have import as raw data in svg formate with draw function(variable name.draw()).
 sk.draw()
 
