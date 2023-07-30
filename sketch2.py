eccd21c7e0366177b1ff663ccc1948a8f23f728e
Govind Saini <79481087+Govindfw220955@users.noreply.github.com>
2023-07-30 11:43:01 +0530

    Update sketch2.py

--- sketch2.py
+++ sketch2.py
@@ -1,3 +1,8 @@
+# Imported the required libraries from the sketchpy library
 from sketchpy import library
+
+# For this sketch we took inbuild sketch module of IronMan as ASCII format.
 myObject = library.ironman_ascii()
+
+# run the sketch program by using draw function.
 myObject.draw()
